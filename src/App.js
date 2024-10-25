d9df823610b0b9919b610a9563d806f65733b95c
Kirill Shakin <shakinkirill@mail.ru>
2024-10-25 15:24:28 +0300

    Настроить проект

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<>
+			<Div>
+				<i className="fa fa-camera-retro"></i>
+				<div>456</div>
+			</Div>
+		</>
+	);
+};
