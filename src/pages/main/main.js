98e24054d1982989bc7054579e020290c1dcbc09
Kirill <shakinkirill@gmail.com>
2024-11-28 17:51:41 +0300

    реализовать пагинацию на главной

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from 'react';
-import { PostCard } from './components';
+import { PostCard, Pagination } from './components';
 import { useServerRequest } from '../../hooks';
+import { PAGINATION_LMIT } from '../../constants';
 import styled from 'styled-components';
+import { getLastPageFromLinks } from './utils';
 
 const MainContainer = ({ className }) => {
 	const [posts, setPosts] = useState([]);
+	const [page, setPage] = useState(1);
+	const [lastPage, setLastPage] = useState(1);
 	const requestServer = useServerRequest();
 	useEffect(() => {
-		requestServer('fetchPosts').then((posts) => {
-			setPosts(posts.res);
-		});
-	}, [requestServer]);
+		requestServer('fetchPosts', page, PAGINATION_LMIT).then(
+			({ res: { posts, links } }) => {
+				setPosts(posts);
+				setLastPage(getLastPageFromLinks(links));
+			},
+		);
+	}, [requestServer, page]);
 
 	return (
 		<div className={className}>
@@ -26,6 +33,7 @@ const MainContainer = ({ className }) => {
 					/>
 				))}
 			</div>
+			{lastPage > 1 && <Pagination setPage={setPage} page={page} lastPage={lastPage} />}
 		</div>
 	);
 };
@@ -34,6 +42,6 @@ export const Main = styled(MainContainer)`
 	& .post-list {
 		display: flex;
 		flex-wrap: wrap;
-        padding: 20px;
+		padding: 20px;
 	}
 `;
